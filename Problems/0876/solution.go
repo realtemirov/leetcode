06cc18b8a5256c96734f4b44c6dd3a4b0c76eedb
realtemirov <temirovjahongir2809@gmail.com>
2025-01-02 20:24:37 +0500

    fix: added test for 876

--- Problems/0876/solution.go
+++ Problems/0876/solution.go
@@ -1,1 +1,14 @@
 package problem0876
+
+import "github.com/realtemirov/leetcode/helper"
+
+func MiddleNode(head *helper.ListNode) *helper.ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
--- Problems/0876/solution_test.go
+++ Problems/0876/solution_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	problem0876 "github.com/realtemirov/leetcode/Problems/0876"
+	"github.com/realtemirov/leetcode/helper"
 	"github.com/stretchr/testify/require"
 )
 
@@ -11,28 +12,23 @@ func TestSolution(t *testing.T) {
 	testCases := []struct {
 		name     string
 		cases    []int
-		expected int
+		expected []int
 	}{
 		{
 			name:     "Test 1",
-			cases:    []int{},
-			expected: 0,
+			cases:    []int{1, 2, 3, 4, 5},
+			expected: []int{3, 4, 5},
 		},
 		{
 			name:     "Test 2",
-			cases:    []int{},
-			expected: 4,
-		},
-		{
-			name:     "Test 3",
-			cases:    []int{},
-			expected: 0,
+			cases:    []int{1, 2, 3, 4, 5, 6},
+			expected: []int{4, 5, 6},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := problem0876.Solution(tc.cases)
+			result := helper.ListToSlice(problem0876.MiddleNode(helper.SliceToList(tc.cases)))
 			require.Equal(t, tc.expected, result, "expected: %v, result: %v", tc.expected, result)
 		})
 	}
